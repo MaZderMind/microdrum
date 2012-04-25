f0c7fc9985acfd04a2b784c17d22c45e5a3496f1
MaZderMind <peter@mazdermind.de>
2012-04-25 23:07:09 +0200

    IO-Komponente für die Parameter-Boards aus dem entsprechenden Testcase

---
+++ Software/Firmware/io_parameter.h
@@ -0,0 +1,11 @@
+#ifndefIO_PARAMETER_H_
+#defineIO_PARAMETER_H_
+
+#defineN_PARAMETERS16
+
+externuint8_tparameter[N_PARAMETERS];
+
+voidio_parameter_init();
+voidio_parameter_sync(uint8_tcycle);
+
+#endif/*IO_PARAMETER_H_*/
