8be99429c1cafc211fef90527c14da21d340e832
MaZderMind <github@mazdermind.de>
2012-05-06 13:42:26 +0200

    Hauptseite der Dokumentation

---
+++ Software/Firmware/mainpage.h
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Dieser Pseudo-Header stellt die Haupseite der Dokumentation bereit
+ */
+
+/**
+ * @mainpage Microdrum Firmware Dokumentation
+ * Dies ist der Sourcecode des Microdum-Projektes (http://microdrum.wordpress.com/).
+ * Die Hardware-Spezifikationen und Software-Komponenten sind hierin veröffentlicht,
+ * Hintergründe und Geschichte können im Blog (http://microdrum.wordpress.com/) nachgelesen werden.
+ * 
+ * @section Repository
+ * Der aktuellen Stand der Quelltexte kann bei GitHub (https://github.com/MaZderMind/microdrum) herunter geladen werden.
+ * 
+ * @section Kontakt
+ * Wenn du Fragen hast, melde dich bei microdrum@mazdermind.de oder benutze das Github-Nachrichten-System
+ */
+
