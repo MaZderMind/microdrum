8fd3ace898ce8bd8795639930067a8deee157e43
MaZderMind <github@mazdermind.de>
2012-05-06 17:43:22 +0200

    Dokumentation der Selektorrad-Routinen

--- Software/Firmware/io_selector.c
+++ Software/Firmware/io_selector.c
@@ -1,43 +1,69 @@
+/**
+ * @file
+ * Auslesen des Selektorrades
+ */
+
 #include <stdlib.h>
+#include "bits.h"
 #include "io_selector.h"
-#include "lcd.h"
 
+/**
+ * Event-Handler für das Niederdrücken des Selektorrads
+ */
 io_selector_pressed_handler pressed_callback;
-io_selector_released_handler released_callback;
-io_selector_left_handler left_callback;
-io_selector_right_handler right_callback;
 
+/**
+ * Event-Handler für das Loslassen des Selektorrads nach dem Niederdrücken
+ */
+io_selector_released_handler released_callback;
+
+/**
+ * Event-Handler, der aufgerufen wird, wenn das Selektorrad einen Schritt
+ * nach links gedreht wurde
+ */
+io_selector_rotated_handler left_callback;
+
+/**
+ * Event-Handler, der aufgerufen wird, wenn das Selektorrad einen Schritt
+ * nach rechts gedreht wurde
+ */
+io_selector_rotated_handler right_callback;
+
+/**
+ * Zwischenspeicher für aktuellen Status
+ */
 struct {
-	// erstes byte
+	/// Entprell-Zähler des linken Ausgangs des Selektorrades
 	unsigned left:2;
+	
+	/// Entprell-Zähler des rechten Ausgangs des Selektorrades
 	unsigned right:2;
 
-	// zweites byte
+	/// Entprell-Zähler des Tasters
 	unsigned pressed:3;
 } io_selector_state;
 
+/*
+ * Initialisieren des Selektorrades
+ */
 void io_selector_init(void)
 {
 	// Selektorrad-Pins auf Eingang
-	SELECTOR_DDR &= ~((1<<SELECTOR_PD_LEFT) | (1<<SELECTOR_PD_RIGHT) | (1<<SELECTOR_PD_PRESS));
+	CLEARBITS(SELECTOR_DDR, BIT(SELECTOR_PD_LEFT) | BIT(SELECTOR_PD_RIGHT) | BIT(SELECTOR_PD_PRESS));
 
 	// PullUps aktivieren
-	SELECTOR_PORT |= (1<<SELECTOR_PD_LEFT) | (1<<SELECTOR_PD_RIGHT) | (1<<SELECTOR_PD_PRESS);
-
-	pressed_callback = NULL;
-	released_callback = NULL;
-	left_callback = NULL;
-	right_callback = NULL;
-
-	io_selector_state.pressed = 0;
+	SETBITS(SELECTOR_PORT, BIT(SELECTOR_PD_LEFT) | BIT(SELECTOR_PD_RIGHT) | BIT(SELECTOR_PD_PRESS));
 }
 
+/**
+ * Einen Druck des Tasters entprellt erkennen
+ */
 void io_selector_detect_press(void)
 {
-	// anliegender Wert am pin des Schalters
+	// anliegender Wert am Pin des Schalters
 	uint8_t pinvalue = !(SELECTOR_PIN & (1<<SELECTOR_PIN_PRESS));
 
-	// wenn der button noch gedrückt ist
+	// wenn der Taster noch gedrückt ist
 	if(pinvalue)
 	{
 		// intervall-counter bis 7 hoch zählen
@@ -56,65 +82,88 @@ void io_selector_detect_press(void)
 
 	}
 
-	// wenn der button los gelassen ist
+	// wenn der Taster los gelassen ist
 	else
 	{
 		// wenn der intervall-counter 6 erreicht hatte,
 		// bevor der button wieder los gelassen wurde
 		if(io_selector_state.pressed >= 6)
 		{
-				// die callback-routine rufen
+			// die callback-routine rufen
 			if(released_callback) released_callback();
 		}
 
-		// den button als losgelassen speichern
+		// den Taster als losgelassen speichern
 		io_selector_state.pressed = 0;
 	}
 }
 
+/**
+ * Eine Rotation des Rotationsencoders entprellt erkennen
+ */
 void io_selector_detect_rotation(void)
 {
+	// Zustand der beiden Ausgänge des Drehencoders
 	uint8_t
 		left = (SELECTOR_PIN & (1<<SELECTOR_PIN_LEFT)),
 		right = (SELECTOR_PIN & (1<<SELECTOR_PIN_RIGHT));
 
+	// Wenn er linke Ausgang up ist
 	if(left)
 	{
+		// Das Statusregister bis 3 hochzählen
 		if(io_selector_state.left < 3)
 		{
 			io_selector_state.left++;
 		}
 	}
+	
+	// wenn er down ist
 	else
 	{
+		// Das Statusregister auf 0 setzen
 		io_selector_state.left = 0;
 	}
 
+	
+	
+	// Wenn er rechte Ausgang up ist
 	if(right)
 	{
+		// Das Statusregister bis 3 hochzählen
 		if(io_selector_state.right < 3)
 		{
 			io_selector_state.right++;
 		}
 	}
+	
+	// wenn er down ist
 	else
 	{
+		// Das Statusregister auf 0 setzen
 		io_selector_state.right = 0;
 	}
 
+	
+	
+	// Wenn der linke Ausgang min. 3 Zyklen up war und der rechte noch down ist
 	if(io_selector_state.left == 2 && !right)
 	{
-		// der Like kanal ging von low auf high während der rechte noch low ist
+		// den Callback anspringen
 		if(right_callback) right_callback();
 	}
-
-
+	
+	// Wenn der rechte Ausgang min. 3 Zyklen up war und der linke noch down ist
 	else if(io_selector_state.right == 2 && !left)
 	{
+		// den anderen Callback anspringen
 		if(left_callback) left_callback();
 	}
 }
 
+/*
+ * Tastendrücke und Rad-Drehung detektieren
+ */
 void io_selector_detect(void)
 {
 	// Taster-Druck detektieren
@@ -125,23 +174,34 @@ void io_selector_detect(void)
 }
 
 
-
+/*
+ * Den Event-Handler für das Niederdrücken des Selektorrads setzen
+ */
 void io_selector_set_pressed_handler(io_selector_pressed_handler callback)
 {
 	pressed_callback = callback;
 }
 
+/*
+ * Den Event-Handler für das Loslassen des Selektorrads nach dem Niederdrücken setzen
+ */
 void io_selector_set_released_handler(io_selector_released_handler callback)
 {
 	released_callback = callback;
 }
 
-void io_selector_set_left_handler(io_selector_left_handler callback)
+/*
+ * Den Event-Handler für das drehen des Selektorrads um einen Schritt nach links setzen
+ */
+void io_selector_set_left_handler(io_selector_rotated_handler callback)
 {
 	left_callback = callback;
 }
 
-void io_selector_set_right_handler(io_selector_right_handler callback)
+/*
+ * Den Event-Handler für das drehen des Selektorrads um einen Schritt nach rechts setzen
+ */
+void io_selector_set_right_handler(io_selector_rotated_handler callback)
 {
 	right_callback = callback;
 }
--- Software/Firmware/io_selector.h
+++ Software/Firmware/io_selector.h
@@ -1,33 +1,111 @@
+/**
+ * @file
+ * Auslesen des Selektorrades, externes Interface
+ */
+
 #ifndef IO_SELECTOR_H_
 #define IO_SELECTOR_H_
 
 #include <avr/io.h>
 
+/**
+ * Port, an dem der Selektor angeschlossen ist
+ */
 #define SELECTOR_PORT      PORTD
+
+/**
+ * Pin des Ports, an dem der Selektor angeschlossen ist
+ */
 #define SELECTOR_PIN       PIND
+
+/**
+ * Data-Direction-Register des Selektors
+ */
 #define SELECTOR_DDR       DDRD
 
-// Selector-Ports PD4-PD6
+
+
+/**
+ * Port-Bit der linken Seite des Drehencoders
+ */
 #define SELECTOR_PD_LEFT      PD4
+
+/**
+ * Port-Bit der rechten Seite des Drehencoders
+ */
 #define SELECTOR_PD_RIGHT     PD5
+
+/**
+ * Port-Bit des Tasters
+ */
 #define SELECTOR_PD_PRESS     PD6
 
-// Selector-Pins PD4-PD6
+
+
+/**
+ * Pin-Bit der linken Seite des Drehencoders
+ */
 #define SELECTOR_PIN_LEFT      PIND4
+
+/**
+ * Pin-Bit der rechten Seite des Drehencoders
+ */
 #define SELECTOR_PIN_RIGHT     PIND5
+
+/**
+ * Pin-Bit des Tasters
+ */
 #define SELECTOR_PIN_PRESS     PIND6
 
+
+
+/**
+ * Definition eines Event-Handlers für das Niederdrücken des Selektorrads
+ */
+typedef void (*io_selector_pressed_handler)(void);
+
+/**
+ * Definition eines Event-Handlers für das Loslassen des Selektorrads nach dem Niederdrücken
+ */
+typedef void (*io_selector_released_handler)(void);
+
+/**
+ * Definition eines Event-Handler für das Drehen des Selektorrads
+ */
+typedef void (*io_selector_rotated_handler)(void);
+
+
+
+/**
+ * Initialisieren des Selektorrades
+ */
 void io_selector_init(void);
+
+/**
+ * Tastendrücke und Rad-Drehung detektieren
+ */
 void io_selector_detect(void);
 
-typedef void (*io_selector_pressed_handler)(void);
-typedef void (*io_selector_released_handler)(void);
-typedef void (*io_selector_left_handler)(void);
-typedef void (*io_selector_right_handler)(void);
 
+
+/**
+ * Den Event-Handler für das Niederdrücken des Selektorrads setzen
+ */
 void io_selector_set_pressed_handler(io_selector_pressed_handler);
+
+/**
+ * Den Event-Handler für das Loslassen des Selektorrads nach dem Niederdrücken setzen
+ */
 void io_selector_set_released_handler(io_selector_released_handler);
-void io_selector_set_left_handler(io_selector_left_handler);
-void io_selector_set_right_handler(io_selector_right_handler);
+
+/**
+ * Den Event-Handler für das drehen des Selektorrads um einen Schritt nach links setzen
+ */
+void io_selector_set_left_handler(io_selector_rotated_handler);
+
+/**
+ * Den Event-Handler für das drehen des Selektorrads um einen Schritt nach rechts setzen
+ */
+void io_selector_set_right_handler(io_selector_rotated_handler);
 
 #endif /* IO_SELECTOR_H_ */
