03478f4a09a7662d700320c7ff2a256be466b1c5
MaZderMind <github@mazdermind.de>
2012-08-27 21:38:47 +0200

    adopt to newer libc

--- Software/Firmware/instrument_names.h
+++ Software/Firmware/instrument_names.h
@@ -14,16 +14,16 @@
 #include <avr/pgmspace.h>
 
 // Instrumentennamen
-prog_char name0[] PROGMEM = "Alpha";
-prog_char name1[] PROGMEM = "Beta";
-prog_char name2[] PROGMEM = "Gamma";
-prog_char name3[] PROGMEM = "Delta";
-prog_char name4[] PROGMEM = "Epsilon";
-prog_char name5[] PROGMEM = "Zeta";
-prog_char name6[] PROGMEM = "Eta";
-prog_char name7[] PROGMEM = "Theta";
+const char PROGMEM name0[] = "Alpha";
+const char PROGMEM name1[] = "Beta";
+const char PROGMEM name2[] = "Gamma";
+const char PROGMEM name3[] = "Delta";
+const char PROGMEM name4[] = "Epsilon";
+const char PROGMEM name5[] = "Zeta";
+const char PROGMEM name6[] = "Iota";
+const char PROGMEM name7[] = "Kappa";
 
 /// Array mit Pointern auf die Namen
-PROGMEM prog_char *names[] = {name0, name1, name2, name3, name4, name5, name6, name7};
+const char *names[] = {name0, name1, name2, name3, name4, name5, name6, name7};
 
 #endif // INSTRUMENT_NAMES_H_
